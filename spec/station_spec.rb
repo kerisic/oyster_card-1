ad9b53d009d9d330637c5273bef8df684b8d4459
Christine Chee <christinechee88@hotmail.com>
2021-02-10 15:35:32 +0000

    Add new Station class and tests

---
+++ spec/station_spec.rb
@@ -0,0 +1,17 @@
+require 'station'
+
+describe Station do
+  let(:station) {Station.new("Green Park", "zone 1")}
+
+  it 'creates a new station' do
+    expect(station).to be_a Station
+  end
+
+  it 'has a name' do
+    expect(station.name).to eq("Green Park")
+  end
+
+  it 'has a zone' do
+    expect(station.zone).to eq("zone 1")
+  end
+end
